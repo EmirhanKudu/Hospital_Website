bc094f4a4dc7f1171c268b8d249acb4d1eddb850
Emirhan <98919988+EmirhanKudu@users.noreply.github.com>
2024-07-22 14:42:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("feedbackForm");
+ document.addEventListener("DOMContentLoaded", () => {
+     const form = document.getElementById("feedbackForm");
     const messageDiv = document.getElementById("message");
 
     form.addEventListener("submit", (event) => {
-        event.preventDefault();
+         event.preventDefault();
 
-        const name = document.getElementById("name").value;
+         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
-        const feedback = document.getElementById("feedback").value;
+         const feedback = document.getElementById("feedback").value;
 
        
-        messageDiv.style.display = "block";
-        messageDiv.style.color = "green";
-        messageDiv.innerHTML = `
-            Teşekkürler, ${name}! Geri bildiriminiz alınmıştır.<br>
-            <strong>E-posta:</strong> ${email}<br>
+         messageDiv.style.display = "block";
+         messageDiv.style.color = "green";
+         messageDiv.innerHTML = `
+           Teşekkürler, ${name}! Geri bildiriminiz alınmıştır.<br>
+             <strong>E-posta:</strong> ${email}<br>
             <strong>Geri Bildirim:</strong> ${feedback}
-        `;
+       `;
+
+        
 
        
         form.reset();
     });
+    
+ });
+
+document.getElementById('bend').addEventListener('click', function() {
+    fetch('http://localhost:8081/doktorlar')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('result').innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Hata:', error);
+        });
 });
